c721a688b0178c1dbe50a2226ae01d90c842049f
Abdur Rahman BIn Muhammad <arshadhinjr@gmail.com>
2021-11-16 10:04:58 +0600

    commited mongodb

--- index.js
+++ index.js
@@ -19,12 +19,61 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology: true } );
 
-async function services() {
+async function products() {
     try {
         await client.connect();
-        const database = client.db( 'assignment12' );
-        const welcomeCollection = database.collection( 'welcome' );
+        const database = client.db( 'hero' );
+        const productsCollection = database.collection( 'products' );
 
+        // GET API
+        app.get( '/products', async ( req, res ) => {
+            const cursor = productsCollection.find( {} );
+            const products = await cursor.toArray();
+            res.send( products );
+        } );
+
+        // GET Single Service
+        app.get( '/products/:id', async ( req, res ) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId( id ) };
+            const product = await productsCollection.findOne( query );
+            res.json( product );
+        } )
+
+        // POST API
+        app.post( '/products', async ( req, res ) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne( product );
+            res.json( result )
+        } );
+
+        // DELETE API
+        app.delete( '/products/:id', async ( req, res ) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId( id ) };
+            const result = await productsCollection.deleteOne( query );
+            res.json( result );
+        } )
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+products().catch( console.dir );
+
+async function carusel() {
+    try {
+        await client.connect();
+        const database = client.db( 'hero' );
+        const caruselCollection = database.collection( 'carusel' );
+
+        // GET API
+        app.get( '/carusel', async ( req, res ) => {
+            const cursor = caruselCollection.find( {} );
+            const carusel = await cursor.toArray();
+            res.send( carusel );
+        } );
 
     }
     finally {
@@ -32,11 +81,11 @@ async function services() {
     }
 }
 
-services().catch( console.dir );
+carusel().catch( console.dir );
 
 
 app.get( '/', ( req, res ) => {
-    res.send( 'Running my CRUD server , Form My heart, hey you' )
+    res.send( 'Running my server from mongoDB' )
 } )
 
 app.listen( port, () => {
